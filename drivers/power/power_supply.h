41cb5daa3178ec02df5125318962df208bb6c6bb
USBhost <josiahspore@gmail.com>
2015-06-25 22:00:10 -0700

    WAR: WOEKAROUND: boot with TRIGGERS
    Signed-off-by: USBhost <josiahspore@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/power/power_supply.h
+++ drivers/power/power_supply.h
@@ -26,6 +26,7 @@ static inline void power_supply_init_attrs(struct device_type *dev_type) {}
 
 #endif /* CONFIG_SYSFS */
 
+/*
 #ifdef CONFIG_LEDS_TRIGGERS
 
 extern void power_supply_update_leds(struct power_supply *psy);
@@ -33,10 +34,11 @@ extern int power_supply_create_triggers(struct power_supply *psy);
 extern void power_supply_remove_triggers(struct power_supply *psy);
 
 #else
+*/
 
 static inline void power_supply_update_leds(struct power_supply *psy) {}
 static inline int power_supply_create_triggers(struct power_supply *psy)
 { return 0; }
 static inline void power_supply_remove_triggers(struct power_supply *psy) {}
 
-#endif /* CONFIG_LEDS_TRIGGERS */
+// #endif /* CONFIG_LEDS_TRIGGERS */
